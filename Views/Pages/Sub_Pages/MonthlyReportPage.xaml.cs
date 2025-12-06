c07f11f89b0df036e880211dbebd8107c449bcfe
Ashen1919 <2303S0834@saegis.ac.lk>
2025-12-07 00:50:29 +0530

    Design UI of report pages of daily & monthly

--- Views/Pages/Sub_Pages/MonthlyReportPage.xaml.cs
+++ Views/Pages/Sub_Pages/MonthlyReportPage.xaml.cs
@@ -1,30 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using ScottPlot;
+using System;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Toyo_cable_UI.Views.Pages.Sub_Pages;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MonthlyReportPage : Page
 {
     public MonthlyReportPage()
     {
         InitializeComponent();
+        TodayMonthText.Text = "This Month: " + DateTime.Now.ToString("MMMM");
+
+        // Axis AntiAliasing Graph
+        double[] monthlySales = { 12000, 15000, 18000, 14000, 20000, 25000 };
+
+        var bar = MyPlotControl.Plot.Add.Bars(monthlySales);
+
+        // Add month labels
+        MyPlotControl.Plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(
+            new Tick[]
+            {
+        new(0, "Acc. Cable"),
+        new(1, "Three-Wheel Cable"),
+        new(2, "Bike Cable"),
+        new(3, "Spark Plug"),
+        new(4, "Lights"),
+        new(5, "Side Mirror"),
+            });
+
+        //disable on-scroll zoom-in and zoom-out
+        MyPlotControl.UserInputProcessor.IsEnabled = false;
+
+        MyPlotControl.Refresh();
     }
 }
