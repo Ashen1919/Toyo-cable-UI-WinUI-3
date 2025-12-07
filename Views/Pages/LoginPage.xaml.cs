e31013a5dc00618517513b3d89e94ad4c48617f7
Ashen1919 <2303S0834@saegis.ac.lk>
2025-12-07 14:32:48 +0530

    update code

--- Views/Pages/LoginPage.xaml.cs
+++ Views/Pages/LoginPage.xaml.cs
@@ -1,26 +1,10 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Toyo_cable_UI.Views.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class LoginPage : Page
     {
         public LoginPage()
@@ -68,6 +52,16 @@ namespace Toyo_cable_UI.Views.Pages
                 _ = dialog.ShowAsync();
                 userNameBox.Text = "";
                 passwordBox.Password = "";
+            }
+        }
+
+        // Trigger with enter key
+        private void PasswordBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                loginBtn_Click(sender, new RoutedEventArgs());
+                e.Handled = true;
             }
         }
     }
